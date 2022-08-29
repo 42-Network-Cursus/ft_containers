15ffd9d24ba6a9ff92580330481ea063cc19f95b
cwastche <marvin@42.fr>
2022-08-29 22:09:54 +0200

    WIP

---
+++ ft_containers.hpp
@@ -0,0 +1,14 @@
+#ifndef FT_CONTAINERS_HPP
+# define FT_CONTAINERS_HPP
+
+# include "vector.hpp"
+
+namespace ft
+{
+	class vector;
+	// class map;
+	// class stack;
+	// class set;
+}
+
+#endif
---
+++ vector.hpp
@@ -0,0 +1,13 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+# include <memory>
+
+template<typename T, typename Alloc = allocator<T>> 
+class vector
+{
+
+};
+
+
+#endif
